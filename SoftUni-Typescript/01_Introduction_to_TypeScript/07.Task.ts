126c70efc538f327c7db98858f50192e145a6c5d
icovv <hvasilev00@abv.bg>
2025-05-20 22:45:51 +0300

    Task 07 SoftUni Typescript course (Introduction to Typescript)

---
+++ SoftUni-Typescript/01_Introduction_to_TypeScript/07.Task.ts
@@ -0,0 +1,14 @@
+function unknownResponse(arg:unknown):{}{
+    if((arg as any).value && typeof (arg as any).value === "string"){
+        return (arg as any).value;
+    } else{
+        return "-"
+    }
+    
+}
+console.log(unknownResponse({ code: 200, text: 'Ok', value: [1, 2, 3] }));
+console.log(unknownResponse({ code: 301, text: 'Moved Permanently', value: 'New Url' }));
+console.log(unknownResponse({ code: 201, text: 'Created', value: { name: 'Test', age: 20 }}));
+console.log(unknownResponse({ code: 201, text: 'Created', value: 'Object Created' }));
+console.log(unknownResponse({ code: 404, text: 'Not found'}));
+console.log(unknownResponse({ code: 500, text: 'Internal Server Error'}));
