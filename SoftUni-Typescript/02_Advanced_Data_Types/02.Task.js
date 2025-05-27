ff85d982425901016289b38bd2f5142a6ca90c34
icovv <hvasilev00@abv.bg>
2025-05-27 21:41:20 +0300

    Task 02 SoftUni Typescript course (Advanced Data Types)

---
+++ SoftUni-Typescript/02_Advanced_Data_Types/02.Task.js
@@ -0,0 +1,16 @@
+function operator(param, operation, operand) {
+    if (operation === "Index" && typeof param !== "number") {
+        return param[operand];
+    }
+    if (operation === "Length" && typeof param !== "number") {
+        return param.length % operand;
+    }
+    if (operation === "Add" && !Array.isArray(param)) {
+        return Number(param) + operand;
+    }
+}
+console.log(operator(['First', 'Second', 'Third'], 'Index', 1));
+console.log(operator('string', 'Index', 1));
+console.log(operator(['Just', 'Two'], 'Length', 5));
+console.log(operator('short string1', 'Length', 5));
+console.log(operator('7', 'Add', 3));
