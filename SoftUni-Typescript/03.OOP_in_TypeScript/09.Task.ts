46457cd5ae60ff92d03dc0f5f9096deab1d390c7
icovv <hvasilev00@abv.bg>
2025-05-31 19:58:33 +0300

    Task 09 SoftUni Typescript course (OOP in TypeScript)

---
+++ SoftUni-Typescript/03.OOP_in_TypeScript/09.Task.ts
@@ -0,0 +1,45 @@
+abstract class Shape{
+    color:string;
+
+    constructor(color:string){
+        this.color = color;
+    }
+
+    abstract getArea():number;
+}
+
+class Circle extends Shape{
+    radius:number;
+
+    constructor(color:string, radius:number){
+        super(color);
+        this.radius = radius;
+    }
+
+    getArea(): number {
+        return Math.PI * this.radius * this.radius
+    }
+}
+
+class Rectangle extends Shape{
+    sideA:number;
+    sideB:number;
+
+    constructor(color:string, sideA:number, sideB:number){
+        super(color);
+        this.sideA = sideA;
+        this.sideB = sideB;
+    }
+
+    getArea(): number {
+        return this.sideA * this.sideB
+    }
+}
+
+const circle = new Circle("red", 5);
+
+console.log(circle.getArea());
+
+const rectangle = new Rectangle("blue", 4, 6);
+
+console.log(rectangle.getArea());
